a9534856e0374e4075d456035f663c19b228adf7
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-28 12:48:55 +0100

    Added a test for user search

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -7,7 +7,27 @@ describe('GitUserSearchController', function(){
   }));
   it('Initialises with an empty search result and term', function(){
     expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();  
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
+  describe('when seaching for a user', function(){
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function(){
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
   });
 
 });
+
