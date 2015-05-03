11991f9afc1b82b74be6d6c5fd30f93550795fab
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-05-04 00:04:30 +0100

    Added more protractor tests

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,6 +1,26 @@
 describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
-    expect(browser.getTitle()).toEqual('Github user search');
+
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function(){
+    browser.get('http://localhost:8080')
   });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github User Search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  })
+
+    it('finds profiles in a list of profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike01');
+  })
 });
